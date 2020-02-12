64764bcefa7998e39b5c0d3cb771764a90a86d82
ddtoan0512 <ddtoan991205@gmail.com>
2020-02-12 11:09:19 +0700

    Update UI for News and integrated CKEditor

---
+++ public/js/plugins/ckeditor/config.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function(config) {
+//     config.filebrowserBrowseUrl = '/js/plugins/ckfinder/browse.php?opener=ckeditor&type=files';
+// config.filebrowserImageBrowseUrl = '/js/plugins/ckfinder/browse.php?opener=ckeditor&type=images';
+// config.filebrowserFlashBrowseUrl = '/js/plugins/ckfinder/browse.php?opener=ckeditor&type=flash';
+// config.filebrowserUploadUrl = '/js/plugins/ckfinder/upload.php?opener=ckeditor&type=files';
+// config.filebrowserImageUploadUrl = '/js/pluginsckcfinder/upload.php?opener=ckeditor&type=images';
+// config.filebrowserFlashUploadUrl = '/js/plugins/ckfinder/upload.php?opener=ckeditor&type=flash';
+};
--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -6,7 +6,68 @@
   </div>
 
   <div class="row">
+  <div class="col">
+      <div class="card display-inline">
+        <div class="card-body">
+          <form action="/admin/students" method="POST">
+            @csrf
+            <div class="form-group row">
+              <label class="col-sm-2 col-form-label" for="title">Tiêu đề</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                  value="{{ old('title') }}">
+                @error('title')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+              </div>
+            </div>
+            
+            <div class="form-group row">
+              <label class="col-sm-2 col-form-label" for="content"> Nội dung</label>
+              <div class="col-sm-10">
+                <textarea name="txtContent" class="form-control " id="txtContent" name="content" rows="10"></textarea>
+              </div>
+              @error('content')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div> 
 
+            <div class="form-group row">
+              <label class="col-sm-2 col-form-label" for="name">Thể loại</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type"
+                  value="{{ old('type') }}">
+                @error('type')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+              </div>
+            </div>
+
+            <div class="form-group text-center">
+              <button type="submit" class="btn btn-primary">Đăng bài</button>
+              <a href="/admin/students" class="btn btn-secondary">Cancel</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
+
+  <script src="{{asset('js/plugins/ckeditor/ckeditor.js')}}"></script>
+  <script src="{{asset('js/plugins/ckfinder/ckfinder.js')}}"></script>
+  <script>
+    CKEDITOR.replace('txtContent', {
+    height: 1500,
+    filebrowserBrowseUrl: "{{asset('js/plugins/ckfinder/ckfinder.html')}}",
+    filebrowserUploadUrl: "{{asset('js/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&amp;type=Files')}}"});
+    
+  </script>
+
 @endsection
