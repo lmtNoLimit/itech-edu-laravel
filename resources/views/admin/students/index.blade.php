7e9daa07bc0f5019c595dd3a18bad3929dc2ca02
Lam Thanh <lmt151099@gmail.com>
2020-02-12 11:58:25 +0700

    fix show student list when no student

--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -53,16 +53,16 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Bạn có chắc muốn xoá?</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Bạn có chắc chắn muốn xoá?</h5>
         <button class="close" type="button" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
         </button>
       </div>
       <div class="modal-footer">
-        <form class="form-inline" action="/admin/students/{{$user->id}}" method="POST" id="delete-form">
+        <form class="form-inline" action="/admin/students/{{$user->id ?? ''}}" method="POST" id="delete-form">
           @csrf
           @method('DELETE')
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <button class="btn btn-secondary mr-2" type="button" data-dismiss="modal">Cancel</button>
           <button class="btn btn-danger" type="submit">Delete</button>
         </form>
       </div>
