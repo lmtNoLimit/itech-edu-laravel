77d0bd00580e488bfe4b100e99b4be4f25ae2e2e
Lam Thanh <lmt151099@gmail.com>
2020-02-13 17:55:27 +0700

    Code home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,211 @@
 
 @section('content')
 <div class="container">
-  <div class="row justify-content-center">
-  </div>
+  <section id="images-slider">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="{{ asset('images/banner/banner_main.jpg') }}" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="{{ asset('images/banner/banner_khachsan.jpg') }}" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="{{ asset('images/banner/banner_ngoaingu.jpg') }}" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="{{ asset('images/banner/banner_oto.jpg') }}" alt="Third slide">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="nganh-dao-tao" class="my-3" style="background: #fff">
+    <div class="row no-gutters">
+      <div class="col-md-3">
+        <div class="card border-0">
+          <a class="text-white" href="#">
+            <img src="{{ asset('images/cntt.jpg') }}" alt="Công nghệ thông tin" width="100%">
+            <div class="card-img-overlay d-flex align-items-end">
+              <p class="card-text h5">CÔNG NGHỆ THÔNG TIN</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-6 p-3">
+        <h2 class="text-center">Các Ngành Đào Tạo</h2>
+        <p class="text-center" style="font-size: 1.3rem">Chương trình đào tạo của viên I-TECH được thiết kế dựa trên sự
+          tham vấn của
+          chuyên
+          gia,chú trọng thực hành
+          , phù hợp với nhu cầu mà nhà tuyển dụng tìm kiếm ở nguồn nhân lực thế hệ mới</p>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-0">
+          <a class="text-white" href="#">
+            <img src="{{ asset('images/an_toan_thong_tin.jpg') }}" alt="An toàn thông tin" width="100%">
+            <div class="card-img-overlay d-flex align-items-end">
+              <p class="card-text h5">AN TOÀN THÔNG TIN</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="row no-gutters">
+      <div class="col-md-3">
+        <div class="card border-0 mb-3">
+          <a class="text-white" href="#">
+            <img src="{{ asset('images/lap_trinh_ai.jpg') }}" alt="Du lịch khách sạn" width="100%">
+            <div class="card-img-overlay d-flex align-items-end">
+              <p class="card-text h5">LẬP TRÌNH AI</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-0">
+          <a class="text-white" href="#">
+            <img src="{{ asset('images/du_lich_khach_san.jpg') }}" alt="Du lịch khách sạn" width="100%">
+            <div class="card-img-overlay d-flex align-items-end">
+              <p class="card-text h5">DU LỊCH KHÁCH SẠN</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-0">
+          <a class="text-white" href="#">
+            <img src="{{ asset('images/ngon_ngu.jpg') }}" alt="Ngôn ngữ" width="100%">
+            <div class="card-img-overlay d-flex align-items-end">
+              <p class="card-text h5">NGÔN NGỮ</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card border-0">
+          <a class="text-white" href="#">
+            <img src="{{ asset('images/cong_nghe_oto.jpg') }}" alt="Công nghệ ô tô" width="100%">
+            <div class="card-img-overlay d-flex align-items-end">
+              <p class="card-text h5">CÔNG NGHỆ Ô TÔ</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="" class="mb-3">
+    <div class="row">
+      <div class="col-12">
+        <img src="{{ asset('images/theo_hoc_tai_itech.jpg')}}" alt="Theo hoc tai itech" width="100%">
+      </div>
+    </div>
+  </section>
+
+  <section id="tuyen-sinh" class="bg-light">
+    <div class="row" style="padding: 0 1rem;">
+      <div class="col-lg-6 col-md-12 my-3">
+        <h2>TUYỂN SINH</h2>
+        <div class="card border-0 mb-3">
+          <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap" height="300">
+          <div class="card-body bg-light px-0">
+            <h5 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, nesciunt impedit!
+              Voluptatibus.</h5>
+            <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi delectus molestiae
+              necessitatibus ex ducimus pariatur esse cumque optio, officiis, minus quas, cupiditate possimus quis
+              accusantium laboriosam! Quas impedit ipsam rem..</p>
+          </div>
+        </div>
+        <div class="d-flex justify-content-center mt-3">
+          <a href="#" class="btn btn-outline-warning">XEM TẤT CẢ</a>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-12 my-3">
+        <h2>CÂU HỎI</h2>
+        <div style="max-height: 500px; overflow: auto">
+          <ul class="list-group">
+            <li class="list-group-item">1. Điều kiện tuyển sinh của Viện Đào tạo CNTT Quốc tế I-Tech là gì?</li>
+            <li class="list-group-item">2. Trình độ Tiếng Anh chưa tốt có đủ điều kiện học tại Viện Đào tạo CNTT Quốc
+              tế
+              I-Tech không?</li>
+            <li class="list-group-item">3. Khi đã có người thân học tại Viện Đào tạo CNTT Quốc tế I-Tech thì khi nhập
+              học
+              có được giảm học phí không?</li>
+            <li class="list-group-item">4. Trong chương trình học có kỳ thực tập doanh nghiệp không? Em sẽ được tự chọn
+              chỗ thực tập không hay nhà
+              trường sắp xếp? Và thực tập có lương hay không?</li>
+            <li class="list-group-item"> 5. Tại Viện Đào tạo CNTT Quốc tế I-Tech, mỗi lớp có bao nhiêu sinh viên?</li>
+            <li class="list-group-item"> 6. Ngành Quản trị và bảo mật hệ thống mạng sinh viên học tại Viện Đào tạo CNTT
+              Quốc tế I-Tech có điểm gì nổi
+              bật so với những Trường khác?</li>
+            <li class="list-group-item"> 7. Học lập trình tại Viện Đào tạo CNTT Quốc tế I-Tech ra trường làm những công
+              việc gì?</li>
+            <li class="list-group-item"> 8. Giữa Phần mềm và An toàn thông tin, ngành nào khó học hơn, ngành nào dễ kiếm
+              việc hơn?</li>
+            <li class="list-group-item"> 9. Ngành Thiết kế đồ họa sinh viên học tại Viện Đào tạo CNTT Quốc tế I-Tech có
+              nơi để thực tập không?</li>
+            <li class="list-group-item">10. Sinh viên học tại Viện Đào tạo CNTT Quốc tế I-Tech có thể đi du học ở nước
+              ngoài được không?</li>
+          </ul>
+        </div>
+        <div class="d-flex justify-content-center mt-3">
+          <a href="#" class="btn btn-outline-success">XEM TẤT CẢ</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="features" class="mt-3">
+    <div class="row px-3">
+      <div class="col-lg p-3 bg-light">
+        <a href="#" class="feature-item">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+            <i class="fas fa-toilet-paper fa-3x my-2"></i>
+            <p class="text-center">BÀI TEST CHỌN NGÀNH NGHỀ</p>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg p-3 bg-success">
+        <a href="" class="feature-item">
+          <div class="d-flex justify-content-center align-items-center flex-column text-white">
+            <i class="fas fa-pencil-alt fa-3x  my-2"></i>
+            <p class="text-center">ĐĂNG KÝ TƯ VẤN</p>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg p-3 bg-light">
+        <a href="#" class="feature-item">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+            <i class="fas fa-search fa-3x my-2"></i>
+            <p class="text-center">TRA CỨU</p>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg p-3 bg-success">
+        <a href="#" class="feature-item">
+          <div class="d-flex justify-content-center align-items-center flex-column text-white">
+            <i class="fas fa-gift fa-3x my-2"></i>
+            <p class="text-center">HỌC BỔNG</p>
+          </div>
+        </a>
+      </div>
+      <div class="col-lg p-3 bg-light">
+        <a href="#" class="feature-item">
+          <div class="d-flex justify-content-center align-items-center flex-column">
+            <i class="fas fa-tv fa-3x my-2"></i>
+            <p class="text-center">ĐĂNG KÝ DỰ TUYỂN ONLINE</p>
+          </div>
+        </a>
+      </div>
+
+    </div>
+  </section>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
   <script src="{{ asset('js/app.js') }}" defer></script>
 
   <!-- Fonts -->
+  <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
 
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -26,16 +27,26 @@
 
 <body>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+    <header id="header" class="navbar navbar-expand-md navbar-dark shadow-sm">
       <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-          LOGO
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        <div class="d-flex">
+          <a class="navbar-brand" href="{{ url('/') }}">
+            <img class="logo" src="{{ asset('images/logo.png') }}" alt="logo">
+          </a>
+          <form class="form-inline mx-4 navbar__form-search">
+            <input class="form-control" type="search" placeholder="Tìm kiếm thông tin..." aria-label="Search">
+            <button class="btn btn-success my-2 my-sm-0 navbar__form-search__button" type="submit">
+              <i class="fas fa-search"></i>
 
+            </button>
+          </form>
+        </div>
+        <button class="navbar-toggler border-0 text-white" type="button" data-toggle="collapse"
+          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <i class="fas fa-bars"></i>
+        </button>
+        {{--
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <!-- Left Side Of Navbar -->
           <ul class="navbar-nav mr-auto">
@@ -48,40 +59,129 @@
             @guest
             <li class="nav-item">
               <a class="nav-link" href="{{ route('login') }}">{{ __('Đăng nhập') }}</a>
-            </li>
-            @if (Route::has('register'))
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">{{ __('Đăng ký') }}</a>
-            </li>
-            @endif
-            @else
-            <li class="nav-item dropdown">
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }} <span class="caret"></span>
-              </a>
+        </li>
+        @if (Route::has('register'))
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('register') }}">{{ __('Đăng ký') }}</a>
+        </li>
+        @endif
+        @else
+        <li class="nav-item dropdown">
+          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }} <span class="caret"></span>
+          </a>
 
-              <div class="dropdown-menu dropdown-menu-right animated faster slideInDown"
-                aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+          <div class="dropdown-menu dropdown-menu-right animated faster slideInDown" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                  {{ __('Đăng xuất') }}
-                </a>
+              {{ __('Đăng xuất') }}
+            </a>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  @csrf
-                </form>
-              </div>
-            </li>
-            @endguest
-          </ul>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          </div>
+        </li>
+        @endguest
+        </ul>
+      </div> --}}
+  </div>
+  </header>
+
+  <nav id="navbar" class="navbar navbar-expand-md">
+    <div class="container">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link px-3" href="#">
+              <i class="fas fa-home fa-lg"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3" href="#">GIỚI THIỆU</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3 dropdown-toggle" data-toggle="dropdown" href="#">
+              <span>NGÀNH ĐÀO TẠO</span>
+            </a>
+            <ul class="dropdown-menu">
+              <li class="nav-item"><a class="nav-link text-dark" href=" #">Cao đẳng chất lượng cao</a></li>
+              <li class="dropdown-divider"></li>
+              <li class="nav-item dropdown-submenu">
+                <a href="#" class="nav-link text-dark">Các khoá học ngắn hạn</a>
+                <ul class="dropdown-menu" style="width: 400px">
+                  <li class="nav-item"><a class="nav-link text-dark href=" #">Giải pháp hạ tầng mạng trong doanh
+                      nghiệp
+                      (CCNA)</a></li>
+                  <li class="dropdown-divider"></li>
+
+                  <a class="nav-link text-dark href=" #">
+                    <li class="nav-item">
+                      Giải pháp dịch vụ mạng trong doanh
+                      nghiệp
+                      (MSNA)
+                    </li>
+                  </a>
+                  <li class="dropdown-divider"></li>
+                  <li class="nav-item"><a class="nav-link text-dark href=" #">Quản trị server Linux</a></li>
+                  <li class="dropdown-divider"></li>
+                  <li class="nav-item"><a class="nav-link text-dark href=" #">Hacker mũ trắng (CEH)</a></li>
+                  <li class="dropdown-divider"></li>
+                  <li class="nav-item"><a class="nav-link text-dark href=" #">Chuyên gia điều tra tội phạm mạng
+                      (CHFI)</a></li>
+                  <li class="dropdown-divider"></li>
+                  <li class="nav-item"><a class="nav-link text-dark href=" #">Lập trình PHP</a></li>
+                  <li class="dropdown-divider"></li>
+                  <li class="nav-item"><a class="nav-link text-dark href=" #">Lập trình Android</a></li>
+                  <li class="dropdown-divider"></li>
+                  <li class="nav-item"><a class="nav-link text-dark href=" #">Thiết kế đồ hoạ</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3" href="#">TUYỂN SINH</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3" href="#">TIN TỨC</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3" href="#">HỘI NHẬP QUỐC TẾ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3" href="#">SINH VIÊN</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link px-3" href="#">LIÊN HỆ</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <main class="main py-4">
+    @yield('content')
+  </main>
+
+  <footer id="footer" class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-6 d-flex justify-content-center">
+          <div class="d-flex flex-column justify-content-between">
+            <p class="h3 text-white">VIỆN ĐÀO TẠO CNTT QUỐC TẾ I-TECH</p>
+            <p class="text-white">Địa chỉ: P301, Nhà C, 290 Tây Sơn - Đống Đa - Hà Nội</p>
+            <p class="text-white">Hotline: 09666.222.76 - 024.666.222.76</p>
+            <p class="text-white">E-mail: info@i-tech.edu.vn</p>
+            <p class="text-white">Website: www.i-tech.edu.vn</p>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 d-flex justify-content-center">
+          <img src="{{ asset('images/ban_do_itech.jpg')}}" alt="ban_do" style="width: 90%">
         </div>
       </div>
-    </nav>
-
-    <main class="py-4">
-      @yield('content')
-    </main>
+    </div>
+  </footer>
   </div>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
