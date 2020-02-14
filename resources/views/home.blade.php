526a34575bd7f015023a848ed8081f9c13019e65
ddtoan0512 <ddtoan991205@gmail.com>
2020-02-14 15:43:08 +0700

    Add facebook comment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -208,5 +208,19 @@
 
     </div>
   </section>
+
+  <section id="facebook">
+			<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.1&appId=297302807515934&autoLogAppEvents=1';
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+			<div class="container">
+				<div class="fb-comments" data-href="http://i-tech.edu.vn/" data-numposts="5" data-width="100%" data-order-by="reverse_time"></div>
+			</div>
+		</section>
 </div>
 @endsection
